a5715009a4c00db8e681a99245f7b728565ec5d2
Iris Li <github@iris.li>
2018-11-18 22:13:23 -0800

    Go bears!

---
+++ precache-manifest.b7a6d0f4b653801c44dd1df67161f398.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f43b8e92b4a81e59a367",
+    "url": "/static/js/main.f43b8e92.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "f43b8e92b4a81e59a367",
+    "url": "/static/css/main.c8167b01.chunk.css"
+  },
+  {
+    "revision": "0d29db57dd04f29856512c0a986fd384",
+    "url": "/index.html"
+  }
+];
